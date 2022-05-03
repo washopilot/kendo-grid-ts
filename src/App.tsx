5e0dfa85a1267b6c5155a94482499d4bdb5f2197
Fernando chicaiza <washopilot@yahoo.com>
2022-05-03 17:30:29 -0500

    instalando dependencias

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
 import React, { Component } from 'react';
-// import './App.css';
+import './App.css';
+
+import '@progress/kendo-theme-default/dist/all.css';
+
+import categories from './categories.json';
+import products from './products.json';
+
+import { process, State } from '@progress/kendo-data-query';
+import {
+  Grid,
+  GridColumn,
+  GridDataStateChangeEvent,
+  GridRowClickEvent,
+  GridCell,
+  GridCellProps,
+} from '@progress/kendo-react-grid';
+import {
+  DropDownList,
+  DropDownListChangeEvent,
+} from '@progress/kendo-react-dropdowns';
+import { Window, WindowActionsEvent } from '@progress/kendo-react-dialogs';
 
 class App extends Component {
   render() {
